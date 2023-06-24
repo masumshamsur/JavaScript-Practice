a851f74fd387f39fe3b5f08feb63f89722fef787
masumshamsur <masumqahelp@gmail.com>
2023-06-24 09:58:25 -0400

    adding occurence exercise

---
+++ occurrence/countNumOccurrence.js
@@ -0,0 +1,14 @@
+const numArr= [4, 9, 2, 4, 7, 9, 4, 9, 2, 4];
+
+const countOccurrence=(arr)=>{
+
+    var count={};  //blank array object
+    for(let i in  arr){
+        count[arr[i]]=count[arr[i]]? count[arr[1]]+1 : 1
+    }
+    
+    return count;
+
+}
+
+console.log("Occurrence- "+countOccurrence(numArr));
