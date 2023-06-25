280931438999dbbb82adddcebcb263f4a88096f9
masumshamsur <masumqahelp@gmail.com>
2023-06-25 12:58:47 -0400

    addding the folder switchCase with exercise

---
+++ switchCase/dayCase.js
@@ -0,0 +1,31 @@
+let day=2;
+let dayname=null;
+
+switch(day){
+    case 1:
+        dayname="Monday";
+        break;
+    case 2:
+        dayname="Tuesday";
+        break;
+    case 3:
+        dayname="Wednesday";
+        break;
+    case 4:
+        dayname="Thursday";
+        break;
+    case 5:
+        dayname="Friday";
+        break;
+    case 6:
+        dayname="Saturday";
+        break;
+    case 7:
+        dayname="Sunday";
+        break;
+    default:
+        dayname= "Invalid day";    
+
+}
+
+console.log(dayname);
